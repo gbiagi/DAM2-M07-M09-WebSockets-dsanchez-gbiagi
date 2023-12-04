5a15d0a81f377f9d2b6c0e64078f45cb167f9303
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2023-12-04 23:39:58 +0100

    flutter app

--- client_flutter/lib/main.dart
+++ client_flutter/lib/main.dart
@@ -1,85 +1,154 @@
+import 'dart:io';
+
+import 'package:client_flutter/memoryGame.dart';
 import 'package:flutter/material.dart';
-import 'package:flip_card/flip_card.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
-    );
-  }
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class LoginPage extends StatelessWidget {
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController serverController = TextEditingController();
+  final TextEditingController portController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _onConnectPressed() {
+    if (_formKey.currentState!.validate()) {
+      // Validation passed, proceed with connection logic
+      String server = serverController.text;
+      String port = portController.text;
+      String name = nameController.text;
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MemoryGame(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('Login Screen'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // TODO: Implement login logic
-            // For simplicity, let's navigate to the game screen directly
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => GameScreen()),
-            );
-          },
-          child: Text('Login'),
+      body: Padding(
+        padding: EdgeInsets.all(100.0),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextFormField(
+                    controller: serverController,
+                    decoration: InputDecoration(labelText: 'Server'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the server';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextFormField(
+                    controller: portController,
+                    decoration: InputDecoration(labelText: 'Port'),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the port';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: InputDecoration(labelText: 'Name'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the name';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _onConnectPressed,
+                  child: Text('Connect'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
 
-class GameScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Memory Game'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Player 1'),
-          Text('Player 2'),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-              ),
-              itemCount: 16,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: FlipCard(
-                    direction: FlipDirection.HORIZONTAL,
-                    front: Card(
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text('Card $index'),
-                      ),
-                    ),
-                    back: Card(
-                      color: Colors.red,
-                      child: Center(
-                        child: Text('.'),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+void main() async {
+  // For Linux, macOS and Windows, initialize WindowManager
+  try {
+    if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+      WidgetsFlutterBinding.ensureInitialized();
+      await WindowManager.instance.ensureInitialized();
+      windowManager.waitUntilReadyToShow().then(showWindow);
+      //windowManager.setSize(const Size(800.0, 600.0));
+    }
+  } catch (e) {
+    // ignore: avoid_print
+    print(e);
   }
+
+  // Define the app as a ChangeNotifierProvider
+  runApp(
+    MaterialApp(
+      title: 'App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    ),
+  );
+}
+
+// Show the window when it's ready
+void showWindow(_) async {
+  windowManager.setMinimumSize(const Size(300.0, 600.0));
+  await windowManager.setTitle('App');
 }
---
+++ client_flutter/lib/memoryGame.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+
+class MemoryGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Widget Example'),
+        ),
+        body: MyWidget(),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double cardSize = 25.0; // Adjust the card size as needed
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Left Text'),
+            Text('Middle Text'),
+            Text('Right Text'),
+          ],
+        ),
+        SizedBox(height: 20),
+        Container(
+          padding: EdgeInsets.only(left: 250, right: 250, top: 5, bottom: 0),
+          child: GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: 16,
+            itemBuilder: (context, index) {
+              return SizedBox(
+                width: cardSize,
+                height: cardSize,
+                child: FlipCard(
+                  direction: FlipDirection.HORIZONTAL,
+                  front: Card(
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        'Front $index',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  back: Card(
+                    color: Colors.red,
+                    child: Center(
+                      child: Text(
+                        'Back $index',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
